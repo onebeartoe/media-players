e926a2e00f05478151b60bdd8c068e7011409541
onebeartoe <onebeartoe@gmail.com>
2015-02-01 11:21:12 -0600

    Cleanup.  The logs are now displayed on the UI.  Mode change functionallity was added.

---
+++ sound-visualizer/src/main/resources/sound-visualizer.js
@@ -0,0 +1,20 @@
+
+function modeChanged(mode)
+{
+//    alert('hiOI');
+    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)      
+        {
+            var s = xmlhttp.responseText + "<br/>" + document.getElementById("logs").innerHTML;
+            document.getElementById("logs").innerHTML = s;
+        }
+    }
+    var param = "sound-meter-mode=" + mode;
+    var url = "/?" + param;    
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("&p=3");
+}
