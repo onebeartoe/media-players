704ec2584913e87925f3a13a139f9e6bd0f952ce
onebeartoe <onebeartoe@gmail.com>
2017-08-14 09:20:23 -0500

    Some documentation was added.

--- jukebox/src/main/java/org/onebeartoe/juke/ui/RandomJuke.java
+++ jukebox/src/main/java/org/onebeartoe/juke/ui/RandomJuke.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// some parts of the JavaFX application were inspired by this media player:
+//      http://fxexperience.com/2012/01/fun-javafx-2-0-audio-player/
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
