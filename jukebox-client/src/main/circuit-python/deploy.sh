93faf93548320f4dab1895b916f0ba4e73139d73
onebeartoe <onebeartoe@gmail.com>
2019-05-11 12:27:14 -0500

    add a state for RandomJuke

--- jukebox-client/src/main/circuit-python/deploy.sh
+++ jukebox-client/src/main/circuit-python/deploy.sh
@@ -1,6 +1,8 @@
 
-cd ~/Versioning/owner/github/onebeartoe/media-players/jukebox-client/src/main/circuit-python
+#cd ~/Versioning/owner/github/onebeartoe/media-players/jukebox-client/src/main/circuit-python
+cd ~/Versioning/owner/github/media-players/jukebox-client/src/main/circuit-python
 
-CIRCUITPY_PATH=/media/roberto/CIRCUITPY/
+#CIRCUITPY_PATH=/media/roberto/CIRCUITPY/
+CIRCUITPY_PATH=/Volumes/CIRCUITPY/
 
 cp main.py $CIRCUITPY_PATH
--- jukebox-client/src/main/circuit-python/main.py
+++ jukebox-client/src/main/circuit-python/main.py
@@ -569,7 +569,7 @@ class Menu_State(State):
                 change_to_state('time')
             elif touch_in_button(t, self.buttons[4]):   # randomjuke
                 logger.debug('randomjuke touched')
-                change_to_state('mugsy')
+                change_to_state('randomjuke')
 #			elif alarm_enabled:
 #                if not self.previous_touch:
 #                    self.previous_touch = t
@@ -614,6 +614,205 @@ class Menu_State(State):
         self.text_areas[1].text = 'randomjuke'
 
 
+
+
+
+class Randomjuke_State(State):
+    """This state manages the primary time display screen/mode"""
+
+    def __init__(self):
+        super().__init__()
+        self.background_day = 'main_background_day.bmp'
+        self.background_night = 'main_background_night.bmp'
+        self.refresh_time = None
+        self.update_time = None
+        self.weather_refresh = None
+        text_area_configs = [dict(x=88, y=170, size=5, color=0xFFFFFF, font=time_font),
+                             dict(x=210, y=50, size=5, color=0xFF0000, font=alarm_font),
+                             dict(x=88, y=90, size=6, color=0xFFFFFF, font=temperature_font)]
+        self.text_areas = create_text_areas(text_area_configs)
+        self.weather_icon = displayio.Group()
+        self.weather_icon.x = 88
+        self.weather_icon.y = 20
+        self.icon_file = None
+
+        self.snooze_icon = displayio.Group()
+        self.snooze_icon.x = 260
+        self.snooze_icon.y = 70
+        self.snooze_file = None
+
+        # each button has it's edges as well as the state to transition to when touched
+        self.buttons = [dict(left=0, top=50, right=80, bottom=120, next_state='settings'),
+                        dict(left=0, top=155, right=80, bottom=220, next_state='menu')]
+
+
+    @property
+    def name(self):
+        return 'randomjuke'
+
+
+    def adjust_backlight_based_on_light(self, force=False):
+        """Check light level. Adjust the backlight and background image if it's dark."""
+        global low_light
+        if light.value <= 1000 and (force or not low_light):
+            pyportal.set_backlight(0.01)
+            pyportal.set_background(self.background_night)
+            low_light = True
+        elif force or (light.value >= 2000 and low_light):
+            pyportal.set_backlight(1.00)
+            pyportal.set_background(self.background_day)
+            low_light = False
+
+
+    def tick(self, now):
+        global alarm_armed, snooze_time, update_time, current_time
+
+        # is the snooze button pushed? Cancel the snooze if so.
+        if not snooze_button.value:
+            if snooze_time:
+                self.snooze_icon.pop()
+            snooze_time = None
+            alarm_armed = False
+
+        # is snooze active and the snooze time has passed? Transition to alram is so.
+        if snooze_time and ((now - snooze_time) >= snooze_interval):
+            change_to_state('alarm')
+            return
+
+        # check light level and adjust background & backlight
+        #self.adjust_backlight_based_on_light()
+
+        # only query the online time once per hour (and on first run)
+        if (not self.refresh_time) or ((now - self.refresh_time) > 3600):
+            logger.debug('Fetching time')
+            try:
+                pyportal.get_local_time(location=secrets['timezone'])
+                self.refresh_time = now
+            except RuntimeError as e:
+                self.refresh_time = now - 3000   # delay 10 minutes before retrying
+                logger.error('Some error occured, retrying! - %s', str(e))
+
+        # only query the weather every 10 minutes (and on first run)
+        if (not self.weather_refresh) or (now - self.weather_refresh) > 600:
+            logger.debug('Fetching weather')
+            try:
+                value = pyportal.fetch()
+                weather = json.loads(value)
+
+                # set the icon/background
+
+                try:
+                	weather_icon_name = weather['weather'][0]['icon']
+                except KeyError:
+                    weather_icon_name = "zzz"
+
+                try:
+                    self.weather_icon.pop()
+                except IndexError:
+                    pass
+                filename = "/icons/"+weather_icon_name+".bmp"
+                if filename:
+                    if self.icon_file:
+                        self.icon_file.close()
+                    self.icon_file = open(filename, "rb")
+                    icon = displayio.OnDiskBitmap(self.icon_file)
+                    try:
+                        icon_sprite = displayio.TileGrid(icon,
+                                                         pixel_shader=displayio.ColorConverter(),
+                                                         x=0, y=0)
+                    except TypeError:
+                        icon_sprite = displayio.TileGrid(icon,
+                                                         pixel_shader=displayio.ColorConverter(),
+                                                         position=(0, 0))
+
+
+                    self.weather_icon.append(icon_sprite)
+
+                try:
+                    temperature = weather['main']['temp'] - 273.15 # its...in kelvin
+                except KeyError:
+					temperature = -100
+
+                if celcius:
+                    temperature_text = '%3d C' % round(temperature)
+                else:
+                    temperature_text = '%3d F' % round(((temperature * 9 / 5) + 32))
+                self.text_areas[2].text = temperature_text
+                self.weather_refresh = now
+                board.DISPLAY.refresh_soon()
+                board.DISPLAY.wait_for_frame()
+
+            except RuntimeError as e:
+                self.weather_refresh = now - 540   # delay a minute before retrying
+                logger.error("Some error occured, retrying! - %s", str(e))
+
+        if (not update_time) or ((now - update_time) > 30):
+            # Update the time
+            update_time = now
+            current_time = time.localtime()
+            time_string = '%02d:%02d' % (current_time.tm_hour,current_time.tm_min)
+            self.text_areas[0].text = time_string
+            board.DISPLAY.refresh_soon()
+            board.DISPLAY.wait_for_frame()
+
+            # Check if alarm should sound
+        if current_time is not None and not snooze_time:
+            minutes_now = current_time.tm_hour * 60 + current_time.tm_min
+            minutes_alarm = alarm_hour * 60 + alarm_minute
+            if minutes_now == minutes_alarm:
+                if alarm_armed:
+                    change_to_state('alarm')
+            else:
+                alarm_armed = alarm_enabled
+
+
+    def touch(self, t, touched):
+        if t and not touched:             # only process the initial touch
+            for button_index in range(len(self.buttons)):
+                b = self.buttons[button_index]
+                if touch_in_button(t, b):
+                    change_to_state(b['next_state'])
+                    break
+        return bool(t)
+
+
+    def enter(self):
+        self.adjust_backlight_based_on_light(force=True)
+        for ta in self.text_areas:
+            pyportal.splash.append(ta)
+        pyportal.splash.append(self.weather_icon)
+        if snooze_time:
+            if self.snooze_file:
+                self.snooze_file.close()
+            self.snooze_file = open('/icons/zzz.bmp', "rb")
+            icon = displayio.OnDiskBitmap(self.snooze_file)
+            try:
+                icon_sprite = displayio.TileGrid(icon,
+                                                 pixel_shader=displayio.ColorConverter(),
+                                                 x=0, y=0)
+            except TypeError:
+                icon_sprite = displayio.TileGrid(icon,
+                                                 pixel_shader=displayio.ColorConverter(),
+                                                 position=(0, 0))
+            self.snooze_icon.append(icon_sprite)
+            pyportal.splash.append(self.snooze_icon)
+        if alarm_enabled:
+            self.text_areas[1].text = '%2d:%02d' % (alarm_hour, alarm_minute)
+        else:
+            self.text_areas[1].text = '     '
+        board.DISPLAY.refresh_soon()
+        board.DISPLAY.wait_for_frame()
+
+
+    def exit(self):
+        super().exit()
+        for _ in range(len(self.snooze_icon)):
+            self.snooze_icon.pop()
+
+
+
+
+
 ####################
 # State management
 
@@ -621,7 +820,8 @@ states = {'time': Time_State(),
           'mugsy': Mugsy_State(),
           'alarm': Alarm_State(),
           'settings': Setting_State(),
-		  'menu' : Menu_State()}
+		  'menu' : Menu_State(),
+          'randomjuke' : Randomjuke_State()}
 
 current_state = None
 
